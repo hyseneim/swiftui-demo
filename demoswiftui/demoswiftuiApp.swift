efa4caa5e23f79c3d38ead4e81bcc322026dec1c
Gabriele Cipolloni <gabriele.cipolloni@capgemini.com>
2023-04-12 13:29:29 +0200

    Initial Commit

---
+++ demoswiftui/demoswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoswiftuiApp.swift
+//  demoswiftui
+//
+//  Created by Gabriele Cipolloni on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct demoswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
